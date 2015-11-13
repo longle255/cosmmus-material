33f1daedbd0b11aad977e61e313e39b65baea2f0
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2015-11-13 10:57:16 +0100

    Added Makefile and script to embed fonts.

---
+++ dynamic-ssmr/embed_fonts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+file=$1
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Using \"dssmr\"."
+    file="dssmr"
+fi
+
+pdf2ps ${file}.pdf && ps2pdf14 -dPDFSETTINGS=/prepress -dEmbedAllFonts=true ${file}.ps ${file}_embedded.pdf && rm ${file}.ps
