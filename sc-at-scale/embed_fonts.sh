0bcf1fb57ffedec3f96d23573c5db531de16bfdf
Eduardo Bezerra <eduardo.bezerra@usi.ch>
2015-12-24 15:49:15 -0300

    No message given for this commit.

--- sc-at-scale/embed_fonts.sh
+++ sc-at-scale/embed_fonts.sh
@@ -6,7 +6,7 @@ file=$1
 if [ -z "$1" ]
   then
     echo "No argument supplied. Using \"dssmr\"."
-    file="dssmr"
+    file="main"
 fi
 
 pdf2ps ${file}.pdf && ps2pdf14 -dPDFSETTINGS=/prepress -dEmbedAllFonts=true ${file}.ps ${file}_embedded.pdf && rm ${file}.ps
